4c7911b2d695f5dfe219cfffe44b5cc1cd7ed36e
cocoper <xuanyang84@gmail.com>
2018-11-29 23:26:18 +0800

    use test_res to save the result of every test

--- main.py
+++ main.py
@@ -2,14 +2,12 @@
 
 import os
 import pickle
-import wx
-import gui
 from Detector import Detector
 from cargobay import CargoBay
 from Environment import Environment
 from time import time
 
-# SD_NUM = 6
+SD_NUM = 6
 TIME_CRI = 60  # in seconds
 
 
@@ -30,22 +28,18 @@ def read_sd(data_path):
 def main():
     # 输入cargobay几何数据
     # 输入SD几何数据
-    app = wx.App(False)
-    MainWnd = gui.MainWindow("CARGO BAY SMOKE DETECTION SYSTEM SIMULATOR")
-    
-    app.MainLoop()
 
-    # predictor = load_model(os.getcwd()+'\\rf_model_all.model')
-    # FWD_cargobay = CargoBay(width=4166, length=16184, height=1727.6)
-    # dets = [Detector(predictor) for i in range(SD_NUM)]
-    # Env1 = Environment(cargobay_obj=FWD_cargobay,
-    #                    detector_series=dets, detector_qty=SD_NUM)
-    # Env1.arrange(fwd_space=100, aft_space=100)
-    # t1 = time()
-    # Env1.run(mode = 'all')
-    # t2 = time()
-    # print('Time used:{:.2f} seconds'.format(t2-t1))
-    # Env1.output()
+    predictor = load_model(os.getcwd()+'\\rf_model_all.model')
+    FWD_cargobay = CargoBay(width=4166, length=16184, height=1727.6)
+    dets = [Detector(predictor,name='SD'+str(i)) for i in range(SD_NUM)]
+    Env1 = Environment(cargobay_obj=FWD_cargobay,
+                       detector_series=dets, detector_qty=SD_NUM)
+    Env1.arrange(fwd_space=100, aft_space=100)
+    t1 = time()
+    Env1.run(mode = 'all')
+    t2 = time()
+    print('Time used:{:.2f} seconds'.format(t2-t1))
+    Env1.output()
 
 if __name__ == "__main__":
     main()
