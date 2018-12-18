903b52bff1c9d29414525852a20efa27de697358
Xuan Yang <xuanyang84@gmail.com>
2018-12-18 23:13:48 +0800

    update

--- SimpleGUI.py
+++ SimpleGUI.py
@@ -3,3 +3,10 @@
 import os
 import wx
 import json
+
+
+class MainWindow(wx.Frame):
+    def __init__(self,title,size=(1200,800)):
+        wx.Frame.__init__(self,None,-1,title=title,size=size)
+        
+        
